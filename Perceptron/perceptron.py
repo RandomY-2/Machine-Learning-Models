ee80d771d7b1a79992f302213df3af84d5b2d0a8
yjh020711@ucla.edu <yjh020711@ucla.edu>
2021-12-13 15:55:32 -0800

    update perceptron to pandas / numpy computing

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -2,31 +2,33 @@ import numpy as np
 import pandas as pd
 
 class Perceptron:
-    def __init__(self, shape, lr=0.01, iterations=1000):
-        self.w = np.ones(shape - 1, dtype=np.float32)
-        self.b = 0
+    def __init__(self, lr=0.001, iterations=1000):
         self.lr = lr
         self.iterations = iterations
-
-    def sign(self, x, w, b):
-        return np.dot(x, w) + b
-
-    # Stochastic Gradient Descent
+        
+    def sign(self, X, w, b):
+        return np.dot(X, w) + b
+    
     def fit(self, X_train, y_train):
-        fitted = False
-        iteration = 0
-        while (not fitted) and (iteration < self.iterations):
-            error_count = 0
-            self.iterations += 1
-            for i in range(len(X_train)):
-                X = X_train[i]
-                y = y_train[i]
-                y_pred = self.sign(X, self.w, self.b)
-
-                if y * y_pred < 0:
-                    self.w = self.w + self.lr * y * X
-                    self.b = self.b + self.lr * y
-                    error_count += 1
-
-            if error_count == 0:
-                fitted = True
+        if isinstance(X_train, pd.core.frame.DataFrame):
+            X_train = X_train.to_numpy()
+        if isinstance(y_train, pd.core.series.Series):
+            y_train = y_train.to_numpy()
+        
+        self.w = np.ones(X_train.shape[1])
+        self.b = 0
+        
+        for iteration in range(self.iterations):
+            for i in range(X_train.shape[0]):
+                y_pred = self.sign(X_train[i], self.w, self.b)
+                
+                if (-1) * y_train[i] * y_pred >= 0:
+                    self.w += self.lr * y_train[i] * X_train[i]
+                    self.b += self.lr * y_train[i]
+                
+    def predict(self, X_test):
+        if (isinstance(X_test, pd.core.frame.DataFrame)):
+            X_test = X_test.to_numpy()
+        
+        pred = self.sign(X_test, self.w, self.b)
+        return np.where(pred >= 0, 1, -1)
